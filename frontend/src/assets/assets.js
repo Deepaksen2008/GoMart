6a00dd3615fd15516df3cdbdb068d1fc60f69725
Deepak Sen <155048041+Deepaksen2008@users.noreply.github.com>
2024-06-24 11:22:18 +0530

    add images

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,178 @@
+
+import menu_1 from './glass-bowl-milk-eggs-bread-260nw-361668512.webp';
+import menu_2 from './fruits.jpg'
+import menu_3 from './Vegetables.jpg'
+import menu_4 from './juice-cold-drink.png'
+import menu_5 from './coockies.png'
+import menu_6 from './icecream.jpg'
+import menu_7 from './pngtree-chocolate-gourmet-drops-png-image_9136898.png'
+import menu_8 from './persnalcare.webp'
+import menu_9 from './attarice.png'
+import menu_10 from './perfume.webp'
+import menu_11 from './medicine.png'
+import play_store from './Screenshot1.png'
+import app_store from './Screenshot2.png'
+import facebook_icon from './Screenshot3.png'
+import twitter_icon from './Screenshot4.png'
+import linkedin_icon from './Screenshot5.png'
+import add_icon_white from './plus.png'
+import remove_icon_red from './minus.png'
+import add_icon_green from './plus.png'
+import rating_starts from './rating.png'
+import cross_icon from './close.png'
+import logo from './logo.jpg'
+import search_icon from './search.png'
+import basket_icon from './basket_icon.png'
+import profile_icon from './user.png'
+import logout_icon from './arrow.png'
+import myorder from './shopping-bag.png'
+import parcel_icon from './complete.png'
+
+export const assets = {
+    
+    play_store,
+    app_store,
+    facebook_icon,
+    twitter_icon,
+    linkedin_icon,
+    add_icon_white,
+    remove_icon_red,
+    add_icon_green,
+    rating_starts,
+    cross_icon,
+    logo,
+    search_icon,
+    basket_icon,
+    profile_icon,
+    logout_icon,
+    myorder,
+    parcel_icon
+
+}
+
+
+export const food_list = [
+    {
+        _id:"1",
+        name:"Greek Salad",
+        image:menu_1,
+        price:12,
+        description:"Food provide essential nutrients",
+        category:"Salad"
+    },{
+        _id:"2",
+        name:"Greek Salad Veg",
+        image:menu_2,
+        price:10,
+        description:"Food provide essential nutrients",
+        category:"Salad34"
+    },{
+        _id:"3",
+        name:"Greek Salad non-veg",
+        image:menu_3,
+        price:52,
+        description:"Food provide essential nutrients",
+        category:"Salad Veg"
+    },{
+        _id:"1",
+        name:"Greek Salad",
+        image:menu_1,
+        price:12,
+        description:"Food provide essential nutrients",
+        category:"Salad"
+    },{
+        _id:"2",
+        name:"Greek Salad Veg",
+        image:menu_2,
+        price:10,
+        description:"Food provide essential nutrients",
+        category:"Salad34"
+    },{
+        _id:"3",
+        name:"Greek Salad non-veg",
+        image:menu_3,
+        price:52,
+        description:"Food provide essential nutrients",
+        category:"Salad Veg"
+    },{
+        _id:"1",
+        name:"Greek Salad",
+        image:menu_1,
+        price:12,
+        description:"Food provide essential nutrients",
+        category:"Salad"
+    },{
+        _id:"2",
+        name:"Greek Salad Veg",
+        image:menu_2,
+        price:10,
+        description:"Food provide essential nutrients",
+        category:"Salad34"
+    },{
+        _id:"3",
+        name:"Greek Salad non-veg",
+        image:menu_3,
+        price:52,
+        description:"Food provide essential nutrients",
+        category:"Salad Veg"
+    },{
+        _id:"1",
+        name:"Greek Salad",
+        image:menu_1,
+        price:12,
+        description:"Food provide essential nutrients",
+        category:"Salad"
+    },{
+        _id:"2",
+        name:"Greek Salad Veg",
+        image:menu_2,
+        price:10,
+        description:"Food provide essential nutrients",
+        category:"Salad34"
+    },{
+        _id:"3",
+        name:"Greek Salad non-veg",
+        image:menu_3,
+        price:52,
+        description:"Food provide essential nutrients",
+        category:"Salad Veg"
+    }
+]
+
+
+export const menu_list = [
+    {
+        menu_name : "Dairy, Bread & Eggs",
+        menu_image : menu_1
+    },{
+        menu_name : "Fruits",
+        menu_image : menu_2
+    },{
+        menu_name : "Vegetables",
+        menu_image : menu_3
+    },{
+        menu_name : "Cold Drinks & Juices",
+        menu_image : menu_4
+    },{
+        menu_name : "Breakfast & Instant Food",
+        menu_image : menu_5
+    },{
+        menu_name : "Ice creams",
+        menu_image : menu_6
+    },{
+        menu_name : "Chocolates",
+        menu_image : menu_7
+    },{
+        menu_name : "Personal Care",
+        menu_image : menu_8
+    },{
+        menu_name : "Atta, Rice & Dal",
+        menu_image : menu_9
+    },{
+        menu_name : "Home & Office",
+        menu_image : menu_10
+    },{
+        menu_name : "Pharma & Wellness",
+        menu_image : menu_11
+    }
+]
